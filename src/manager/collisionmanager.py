351931565d25ab0e5086b0cde13066137d0bd030
John Yenter-Briars <jyenterbriars@gmail.com>
2021-12-27 12:33:56 -0600

    mend

--- src/manager/collisionmanager.py
+++ src/manager/collisionmanager.py
@@ -13,50 +13,6 @@ class CollisionManager:
         obj2_in_obj1 = self.obj1_in_obj2_temp(object2, object1, lightning_bolt)
         return obj1_in_obj2 or obj2_in_obj1
 
-    def obj1_in_obj2_temp(self, object1: GameObject, object2: GameObject, lightning_bolt=False) -> bool:
-        """OK listen up boils an ghouls, here's how we're doing collision. It's currently two days before Christmas and I'm hyped up on python so lets go
-
-           Since all objects are squares, simple check each corner of square 1 and see if it's coordinate is bounded by the square of object 2
-           If it's ever bounded, you know you have a collision
-
-           Repeate this process for all 4 corners. Viola. This should handle every case *EXCEPT* when the squares are *EXACTLY* one on top of each other.
-           In that case, according to this algorithm, no collision will be detected. But what are the chances of that happening right?.....?
-        """
-
-        # check if bottom right corner of object 1 is in the square of object 2
-        if  object2.get_left_up_corner().x <= object1.get_right_down_corner().x <= object2.get_right_up_corner().x and \
-            object2.get_left_up_corner().y <= object1.get_right_down_corner().y <= object2.get_left_down_corner().y:
-            if lightning_bolt:
-                print(f"bottom right corner of {type(object1)} is in the square of {type(object2)}")
-                self.debug(object1, object2)
-            return True
-
-        # check if bottom left corner of object 1 is in the square of object 2
-        if  object2.get_left_up_corner().x <= object1.get_left_down_corner().x <= object2.get_right_up_corner().x and \
-            object2.get_left_up_corner().y <= object1.get_left_down_corner().y <= object2.get_right_down_corner().y:
-            if lightning_bolt:
-                print(f"bottom left corner of {type(object1)} is in the square of {type(object2)}")
-                self.debug(object1, object2)
-            return True
-
-        # check if top right corner of object 1 is in the square of object 2
-        if  object2.get_left_down_corner().x <= object1.get_right_up_corner().x <= object2.get_right_down_corner().x and \
-            object2.get_left_up_corner().y <= object1.get_right_up_corner().y <= object2.get_left_down_corner().y:
-            if lightning_bolt:
-                print(f"top right corner of {type(object1)} is in the square of {type(object2)}")
-                self.debug(object1, object2)
-            return True
-
-        # check if top left corner of object 1 is in the square of object 2
-        if  object2.get_left_down_corner().x <= object1.get_left_up_corner().x <= object2.get_right_down_corner().x and \
-            object2.get_right_up_corner().y <= object1.get_left_up_corner().y <= object2.get_right_down_corner().y:
-            if lightning_bolt:
-                print(f"top left corner of {type(object1)} is in the square of {type(object2)}")
-                self.debug(object1, object2)
-            return True
-
-        return False
-
     def obj1_in_obj2(self, object1: GameObject, object2: GameObject, lightning_bolt=False) -> bool:
         """OK listen up boils an ghouls, here's how we're doing collision. It's currently two days before Christmas and I'm hyped up on python so lets go
 
